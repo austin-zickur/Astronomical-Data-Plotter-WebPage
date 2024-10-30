31fdd6a57eeb1ac18e0bb00bef9f94357f371abf
Kylee <kyleebrown.work@gmail.com>
2024-10-30 12:20:00 -0500

    cleaned up upload functionality

--- main.js
+++ main.js
@@ -6,32 +6,38 @@ var upload = document.getElementById("upload");
 var files = document.getElementById("file-inp");
 
 var scrollFile = document.getElementById("scroll-file");
+var uploadText = document.getElementById("up-txt");
 
-// file arrow down function
+/* FILE ARROW DOWN FUNCTION */
 fileArrow.addEventListener("click", function(){
     // if box height is 25% change to 10% else change to 25%
     filebox.style.height = (filebox.style.height === "25%") ? "4%" : "25%";
     // if box height is 25% rotate arrow 90deg else rotate arrow 25deg
     fileArrow.style.rotate = (filebox.style.height === "25%") ? "90deg" : "-90deg";
     scrollFile.style.height = (filebox.style.height === "25%") ? "80%" : "0";
-
-
 });
 
-upload.addEventListener("click", function(){
-   for (const file of files.files) {
+ /* UPLOAD FUNCTION */
+ upload.addEventListener("click", function(){
+    //CREATE IN ELEMENT IN FILE BOX FOR EACH FILE UPLOADED
+    for (const file of files.files) {
         var fileDiv = `
         <div class="file-cont center">
         <img src="images/icons8-file-48.png">
         <p>${file.name}<p>
-        </div>`
+        </div>`;
         
         scrollFile.insertAdjacentHTML("afterbegin", fileDiv)
-        
+        //DEBUG
         console.log(`${file.name}`);
-    
     };
-    
-})
+    //CLEAR the FILE INPUT AFTER UPLOAD
+    files.value = null;
+    console.log(scrollFile.childElementCount);
+    if (scrollFile.childElementCount > 1 ){
+        uploadText.style.display = "none";
+    };
+});
+
 
 
